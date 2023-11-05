621b32bc0ee4664221bdf34d16adf1b27f37e422
Xavier DEGENNE <xavier.degenne@gmail.com>
2023-11-05 15:12:16 +0100

    WIP

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 # stdlib
 from influxdb import InfluxDBClient
 import datetime, time, logging
+import time
+import board
+import adafruit_dht
+import psutil
 
 # Self libraries
 import utils
@@ -19,8 +23,47 @@ period = int(config.get('period', 60))
 client = InfluxDBClient(host='localhost', port=8086)
 client.switch_database('hsh_sensors')
 
+# We first check if a libgpiod process is running. If yes, we kill it!
+for proc in psutil.process_iter():
+    if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+        proc.kill()
+sensor = adafruit_dht.DHT11(board.D17)
+
+def read_sensor():
+    value = None
+    while value == None:
+        try:
+            temp = sensor.temperature
+            humidity = sensor.humidity
+            log.debug(f"Temperature: {temp}*C   Humidity: {humidity}%", temp, humidity)
+            value = [
+                {
+                    "measurement": "dhtEvents",
+                    "tags": {
+                        "sondeId": "D17"
+                    },
+                    "time": datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
+                    "fields": {
+                        "temperature": temp,
+                        "humidity": humidity
+                    }
+                }
+            ]
+        except RuntimeError as error:
+            log.error(error.args[0])
+        except Exception as error:
+            sensor.exit()
+            raise error
+        if value == None:
+            time.sleep(2.0)
+        else:
+            return value
+
 while True:
     log.debug("Cycle begins")
+
+    value = read_sensor()
+    client.write_points(value)
 
     log.debug(f"Cycle ends, sleeping for {period} seconds")
     time.sleep(period)    
